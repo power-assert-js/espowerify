e1c65b0d7fc604940bba5de1642f758133e66502
Takuto Wada <takuto.wada@gmail.com>
2014-05-06 19:20:45 +0900

    Use assert since we encourage simple assert style.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 var espowerify = require('..'),
     fs = require('fs'),
     Stream = require('stream'),
-    expect = require('expect.js');
+    assert = require('assert');
 
 describe('espowerify with default options', function() {
     var stream = espowerify('test/fixtures/example.js');
     
     it('should return a stream', function() {
-        expect(stream).to.be.a(Stream);
+        assert(stream instanceof Stream);
     });
     
     it('should transform module', function(done) {
@@ -17,7 +17,7 @@ describe('espowerify with default options', function() {
         });
         stream.on('end', function() {
             var expected = fs.readFileSync('test/expected/example.js', 'utf8');
-            expect(output).to.be(expected);
+            assert.equal(output, expected);
             done();
         });
         file = fs.createReadStream('test/fixtures/example.js');
@@ -41,7 +41,7 @@ describe('espowerify with customized options', function() {
         });
     
     it('should return a stream', function() {
-        expect(stream).to.be.a(Stream);
+        assert(stream instanceof Stream);
     });
     
     it('should transform module', function(done) {
@@ -51,7 +51,7 @@ describe('espowerify with customized options', function() {
         });
         stream.on('end', function() {
             var expected = fs.readFileSync('test/expected/customized.js', 'utf8');
-            expect(output.toString()).to.be(expected.toString());
+            assert.equal(output, expected);
             done();
         });
         file = fs.createReadStream('test/fixtures/customized.js');
