fc08a686ad362b92022c38b40e7f997d06df2302
Takuto Wada <takuto.wada@gmail.com>
2014-08-22 14:26:32 +0900

    refactor(espowerify): extract functions to be lint-free

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ var through = require('through'),
     esprima = require('esprima'),
     escodegen = require('escodegen'),
     extend = require('xtend'),
-    path = require('path'),
     convert = require('convert-source-map'),
     sm = require('source-map'),
     SourceMapGenerator = sm.SourceMapGenerator,
     SourceMapConsumer = sm.SourceMapConsumer;
-
 
 // borrowed from vinyl-sourcemaps-apply
 function mergeSourceMap(incomingSourceMap, outgoingSourceMap) {
@@ -34,48 +32,52 @@ function mergeSourceMap(incomingSourceMap, outgoingSourceMap) {
     return JSON.parse(generator.toString());
 }
 
-
-function transform (jsCode, filepath, options) {
+function handleUpstreamSourceMap (jsCode, options) {
     var inMap;
-
-    var jsAst = esprima.parse(jsCode, {tolerant: true, loc: true, source: filepath});
-
-    var espowerOptions = extend(espower.defaultOptions(), options, {
-        destructive: true,
-        path: filepath
-    });
-
-    if (espowerOptions.sourceMap) {
-        inMap = espowerOptions.sourceMap;
+    if (options.sourceMap) {
+        inMap = options.sourceMap;
     } else {
         var commented = convert.fromSource(jsCode);
         if (commented) {
             inMap = commented.toObject();
-            espowerOptions.sourceMap = inMap;
+            options.sourceMap = inMap;
         }
     }
+    return inMap;
+}
 
-    var modifiedAst = espower(jsAst, espowerOptions);
-
+function instrument (jsCode, filepath, options) {
+    var jsAst = esprima.parse(jsCode, {tolerant: true, loc: true, source: filepath});
+    var modifiedAst = espower(jsAst, options);
     // keep paths absolute by not using `file` and `sourceMapRoot`
     // paths will be resolved by mold-source-map
-    var escodegenOutput = escodegen.generate(modifiedAst, {
+    return escodegen.generate(modifiedAst, {
         sourceMap: true,
         sourceMapWithCode: true
     });
-    var modifiedCode = escodegenOutput.code;
-    var outMap = convert.fromJSON(escodegenOutput.map.toString());
+}
 
+function mergeEspowerOptions (options, filepath) {
+    return extend(espower.defaultOptions(), options, {
+        destructive: true,
+        path: filepath
+    });
+}
+
+function transform (jsCode, filepath, options) {
+    var espowerOptions = mergeEspowerOptions(options, filepath);
+    var inMap = handleUpstreamSourceMap(jsCode, espowerOptions);
+    var instrumented = instrument(jsCode, filepath, espowerOptions);
+    var outMap = convert.fromJSON(instrumented.map.toString());
     if (inMap) {
         var mergedRawMap = mergeSourceMap(inMap, outMap.toObject());
-        return modifiedCode + '\n' + convert.fromObject(mergedRawMap).toComment() + '\n';
+        return instrumented.code + '\n' + convert.fromObject(mergedRawMap).toComment() + '\n';
     } else {
         outMap.setProperty('sources', [filepath]);
         outMap.setProperty('sourcesContent', [jsCode]);
-        return modifiedCode + '\n' + outMap.toComment() + '\n';
+        return instrumented.code + '\n' + outMap.toComment() + '\n';
     }
 }
-
 
 /**
  * Apply espower through the browserify transform chain.
