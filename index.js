80b1841f02caf9ceac418aa3c69647f6ab8c4029
Takuto Wada <takuto.wada@gmail.com>
2014-10-05 17:28:25 +0900

    feat(espowerify): transform file if and only if it is listed in browserify entries

--- index.js
+++ index.js
@@ -10,7 +10,21 @@
 'use strict';
 
 var through = require('through'),
+    path = require('path'),
     espowerSource = require('espower-source');
+
+function endsWith (str, suffix) {
+    return str.slice(str.length - suffix.length) === suffix;
+}
+
+function isTransformTarget (filepath, options) {
+    if (!options || !options._flags || !options._flags.entries) {
+        return true;
+    }
+    return options._flags.entries.some(function (entryPath) {
+        return endsWith(filepath, path.normalize(entryPath));
+    });
+}
 
 /**
  * Apply espower through the browserify transform chain.
@@ -20,6 +34,9 @@ var through = require('through'),
  * @return {Stream}
  */
 function espowerify(filepath, options) {
+	if (!isTransformTarget(filepath, options)) {
+        return through();
+    }
 
     var data = '',
         stream = through(write, end);
